e8734e1c9cfbec9e496f3973c06d990da68926f2
fernoe1 <krutoytemirlan2007@mail.ru>
2025-10-19 18:25:34 +0600

    feat: cors

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const pseudoDB = require('./database/pseudoDB');
 
 const app = express();
 const PORT = 3000;
+
+app.use(cors());
 
 app.use(express.json());
 
